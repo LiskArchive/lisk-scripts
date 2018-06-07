46c778a6583a62f540e70ab083ee67bc7aff28a6
Nazar Hussain <nazarhussain@gmail.com>
2018-06-07 12:47:46 +0200

    Make the config migration backward compatible

--- downloaded/installLisk.sh
+++ downloaded/installLisk.sh
@@ -265,11 +265,16 @@ upgrade_lisk() {
 	fi
 
 	echo -e "\\nCopying config.json entries from previous installation"
-    if [[ -z "${LISK_MASTER_PASSWORD}" ]]; then
+	# If its 0.9.x version
+	if [ -f "${LISK_INSTALL}/updateConfig.js" ]; then
+	    "$LISK_INSTALL"/bin/node "$LISK_INSTALL"/updateConfig.js -o "$LISK_BACKUP"/config.json -n "$LISK_INSTALL"/config.json
+
+    # If its a latest version
+    elif [[ -z "${LISK_MASTER_PASSWORD}" ]]; then
         "$LISK_INSTALL"/bin/node "$LISK_INSTALL"/scripts/update_config.js "$LISK_BACKUP"/config.json "$LISK_INSTALL"/config.json
     else
         "$LISK_INSTALL"/bin/node "$LISK_INSTALL"/scripts/update_config.js "$LISK_BACKUP"/config.json "$LISK_INSTALL"/config.json --password "$LISK_MASTER_PASSWORD"
-    fi
+	fi
 }
 
 usage() {
