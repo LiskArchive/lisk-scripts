a7b235fa788b70bf0448ecb89db5f51503561959
Nazar Hussain <nazarhussain@gmail.com>
2018-06-07 12:26:36 +0200

    Update the config file path and check for master password

--- downloaded/installLisk.sh
+++ downloaded/installLisk.sh
@@ -265,7 +265,11 @@ upgrade_lisk() {
 	fi
 
 	echo -e "\\nCopying config.json entries from previous installation"
-	"$LISK_INSTALL"/bin/node "$LISK_INSTALL"/updateConfig.js -o "$LISK_BACKUP"/config.json -n "$LISK_INSTALL"/config.json
+    if [[ -z "${LISK_MASTER_PASSWORD}" ]]; then
+        "$LISK_INSTALL"/bin/node "$LISK_INSTALL"/scripts/update_config.js "$LISK_BACKUP"/config.json "$LISK_INSTALL"/config.json
+    else
+        "$LISK_INSTALL"/bin/node "$LISK_INSTALL"/scripts/update_config.js "$LISK_BACKUP"/config.json "$LISK_INSTALL"/config.json --password "$LISK_MASTER_PASSWORD"
+    fi
 }
 
 usage() {
