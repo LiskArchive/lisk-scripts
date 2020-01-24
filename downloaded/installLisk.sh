d43e76d2f0d272c42feef3ae79514f04466ed88f
François Chavant <francois@lightcurve.io>
2020-01-24 16:37:44 +0100

    Bump version to 0.9.0

--- downloaded/installLisk.sh
+++ downloaded/installLisk.sh
@@ -19,7 +19,7 @@ IFS=$'\n\t'
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ######################################################################
 
-# VERSION 0.7.1
+# VERSION 0.9.0
 
 # Variable Declaration
 DEFAULT_LISK_LOCATION=$( pwd )
